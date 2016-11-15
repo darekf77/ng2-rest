f5294dd57c92aaf318eebe616844a52a6bd41c26
Dariusz Filipiak <darekf77@gmail.com>
2016-11-15 13:42:49 +0100

    Release 4.1.43

--- src/providers.ts
+++ src/providers.ts
@@ -2,6 +2,5 @@ import {Resource} from './resource.service';
 import {MockController} from './mock.controller';
 import {MockAutoBackend} from './mock-auto-backend.class';
 import { JsonpModule } from '@angular/http';
-import { LocalStorageEmitter, LocalStorageService } from 'angular2-localstorage/LocalStorageEmitter';
 
-export const NG2REST_PROVIDERS = [Resource, JsonpModule, LocalStorageService];
+export const NG2REST_PROVIDERS = [Resource, JsonpModule];
