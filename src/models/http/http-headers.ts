006bacd75c1749b5f1422f45ebf445f9a0153106
Dariusz Filipiak <darekf77@gmail.com>
2017-01-06 09:32:48 +0100

    Release 5.0.12

--- src/models/http/http-headers.ts
+++
@@ -1,9 +0,0 @@
-export interface Header {
-    [headerName: string]: string;
-}
-
-export interface HttpHeaders<T, TA> {
-    addOrReplace: (header: Header, forever?: boolean) => void;
-    remove: (key: string, forever?: boolean) => void;
-}
-
