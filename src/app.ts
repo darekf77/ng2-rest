fa3b04eb792ee4118fc10b0e9f959710fd0b5b10
Dariusz Filipiak <dariusz.filipiak@igt.com>
2020-10-11 04:28:53 +0200

    new version 11.0.3

--- src/app.ts
+++ src/app.ts
@@ -3,24 +3,19 @@ import { Resource } from 'ng2-rest';
 import { Morphi } from 'morphi';
 
 
-
 const host = 'http://localhost:3000'
 const start = async () => {
   if(Morphi.IsBrowser) {
     const rest = Resource.create(host, '/test');
-
     console.log('hello');
     await rest.model().get();
   }
-
-
-
 }
-
 
 export default start;
 
-
+//#region @notForNpm
 if (Helpers.isBrowser) {
   start()
 }
+//#endregion
