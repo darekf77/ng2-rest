5480d1cfb589fe636755e04eb14a81a7b2dd0591
Dariusz Filipiak <dariusz.filipiak@igt.com>
2021-01-19 20:16:38 +0100

    before replay record finish

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,60 @@
+import * as _ from 'lodash';
 import { Helpers } from './helpers';
 import { Resource } from 'ng2-rest';
 import { Morphi } from 'morphi';
 
+@Morphi.Controller()
+class RestCtrl extends Morphi.Base.Controller<any> {
+  //#region @backend
+  async initExampleDbData() {
 
-const host = 'http://localhost:3000'
-const start = async () => {
-  if(Morphi.IsBrowser) {
+  }
+  //#endregion
+
+  @Morphi.Http.GET()
+  ng(): Morphi.Response<any> {
+    return async () => {
+      return 'ng2-rest perfect!'
+    }
+  }
+}
+
+
+
+
+const start = async (port = 3000) => {
+
+  const host = `http://localhost:${port}`;
+  console.log(`HOST ng2-rest: ${host}`);
+  //#region @backend
+  const config = {
+    type: "sqlite",
+    database: 'tmp-db.sqlite',
+    synchronize: true,
+    dropSchema: true,
+    logging: false
+  };
+  //#endregion
+
+  const context = await Morphi.init({
+    host,
+    controllers: [RestCtrl],
+    entities: [],
+    //#region @backend
+    config: config as any
+    //#endregion
+  });
+
+
+  if (Morphi.IsBrowser) {
     const rest = Resource.create(host, '/test');
     console.log('hello');
     await rest.model().get();
+
+    const c: RestCtrl = _.first(context.controllers);
+    const data = (await c.ng().received).body.text;
+    console.log('ng:', data)
+
   }
 }
 
