bf3abe589d73a5cef53b5d2b444bc736e8b69398
Dariusz Filipiak <darekf77@gmail.com>
2025-08-03 21:28:02 +0200

    release: v19.0.35

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,7 +1,8 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.ng2-rest';
+export const BUILD_BASE_HREF = '';
 export const PROJECT_NPM_NAME = 'ng2-rest';
-export const CURRENT_PACKAGE_VERSION = '19.0.34';
+export const CURRENT_PACKAGE_VERSION = '19.0.35';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
